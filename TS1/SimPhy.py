f51874467929de3ada89bea68c1c1027d46297f6
Fransr24 <pereirafrancisco240501@gmail.com>
2022-07-01 22:33:03 -0300

    Subo Ts5

--- TS1/SimPhy.py
+++ TS1/SimPhy.py
@@ -8,25 +8,25 @@ Created on Sun Apr 17 18:29:43 2022
 import matplotlib.pyplot as plt  
 import numpy as np  #
 import scipy.signal as sig
-from splane import analyze_sys, pretty_print_bicuad_omegayq
+import splane as tc2
 
 w0 = 1
 w1 = -1
 e = 0.508
 
 
-num = np.array([1, 0])
-den = np.array([1, 6, 4])
+num = np.array([ 1, 0, 9])
+den = np.array([ 1, 6, 12, 9])
 
-
-pretty_print_bicuad_omegayq(num,den)
 
 mi_sos = sig.TransferFunction(num,den)
 
+sos_pba = tc2.tf2sos_analog(num,den)
+tc2.pretty_print_SOS(sos_pba)
+
 print (mi_sos)
 plt.close('all')
-%matplotlib qt5
-analyze_sys(mi_sos, 'mi SOS')
+tc2.analyze_sys(mi_sos, 'mi SOS')
 
 
 # # Codigo para probar si a una buena escala funciona bien
